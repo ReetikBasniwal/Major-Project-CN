954527003197f19179339224e13d747d48287163
Hritik kumar <hritikkumar@Reetiks-MacBook-Air.local>
2023-03-27 19:05:23 +0530

    Sign up and Sign In is created

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    name: {
+        type: String,
+        require: true
+    }
+}, {
+    timestampes: true
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
