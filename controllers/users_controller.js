148c58d559dfea3a3f0a5202a4bda7dea3796967
Hritik kumar <hritikkumar@Reetiks-MacBook-Air.local>
2023-04-15 12:29:11 +0530

    post is created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,13 +1,11 @@
  const User = require('../models/user');
- 
- module.exports.profile = function(req, res){
-   return res.render('home', {
-      title: "User Profile"
-  });
- }
 
  // render the sign up page
  module.exports.signUp = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_up', { 
       title: "Codeial | Sign Up"
     });
@@ -16,10 +14,22 @@
 
  // render the sign in page
  module.exports.signIn = function(req, res){
-  return res.render('user_sign_in', {
-      title: "Codeial | Sign In"
-  })
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
+    })
  }
+ 
+ module.exports.profile = function(req, res){
+    return res.render('user_profile', {
+        title: 'User Profile'
+    })
+ }
+
+
+ 
 
  // get the sign up data
  module.exports.create = function(req, res){
@@ -45,5 +55,13 @@
 
  // sign in and create session for user
  module.exports.createSession = function(req, res){
-    //  
+    return res.redirect('/');
 };
+
+module.exports.destroySession = function(req, res){
+    req.logout(function(err) {
+        if (err) { return next(err) };
+    });
+
+    return res.redirect('/');
+}
--- index.js
+++ index.js
@@ -5,10 +5,25 @@ const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
+//used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
+const sassMiddleware = require('node-sass-middleware');
+
+
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
+
 app.use(express.urlencoded());
 
 app.use(cookieParser());
-
 
 app.use(express.static('./assets'));
 
@@ -17,13 +32,41 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-// use express router
-app.use('/', require('./routes'));
 
 // set up the view engine
 app.set('view engine', 'ejs'); 
 app.set('views', './views');
 
+//mongo store is used to store the cookie in the db
+
+app.use(session({
+    name: 'codeail',
+    //To Do change the secret before deployement in production mode 
+    secret: 'blah..something',
+    saveUninitialized: false,
+    resave: false,
+    store: MongoStore.create({mongoUrl:'mongodb://localhost/codeial_development_1'}),
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    }
+    // store: new MongoStore(
+    //     {
+    //         mongooseConnection: db,
+    //         autoRemove: 'disabled'
+    //     },
+    //     function(err){
+    //          console.log(err || 'connect-mongo db setup ok'); 
+    //     }
+    // )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+// use express router
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if(err){
